7dde7256a42cd6523ba8bfdea440531a45b640b7
samiksha0312 <137166610+samiksha0312@users.noreply.github.com>
2024-06-25 21:30:02 +0530

    Update page.tsx

--- PRODIGY_WD_04/src/app/(pages)/contact/page.tsx
+++ PRODIGY_WD_04/src/app/(pages)/contact/page.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
       <section className="mapbox" data-mapbox>
         <figure>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3561.4762695601853!2d87.28978147529052!3d26.79295967671882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19b606ce89f9%3A0x7423e72f8e139e05!2sPurwanchal%20Engineering%20Campus(ERC)!5e0!3m2!1sen!2snp!4v1706846735161!5m2!1sen!2snp"
+            src="https://maps.google.com/maps?ll=21.013994,75.520177&z=13.985886162830512&t=m&hl=en-US&gl=US&mapclient=apiv3&cid=11884075654126657145"
             width="400"
             height="300"
             loading="lazy"
